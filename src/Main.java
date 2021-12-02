4bb0eb941825e5080f0c3698155cf4823675f75c
Dmitriy Tikhonin <dimansky777@gmail.com>
2021-12-02 15:07:07 +0300

    Java project added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2000000000;
+        int transfer = 500000000;
+        int totalSum =  account + transfer;
+        System.out.println(totalSum);
+    }
+}
